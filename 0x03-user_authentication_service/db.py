24534b40e48600909f430b691b0a3fd0b6516c1b
Pasitik <paasitik@gmail.com>
2024-11-23 04:28:59 +0000

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database.
         """
         try:
